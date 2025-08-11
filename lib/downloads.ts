1ed4cc5c3cfe5fdb84ac693cb0de66117496bc17
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-11 15:53:21 -0600

    Create downloads.ts

---
+++ lib/downloads.ts
@@ -0,0 +1,10 @@
+export const DOWNLOADS: Record<string, { filename: string; path: string }> = {
+  'como-apagar-tu-mente': {
+    filename: 'como-apagar-tu-mente.pdf',
+    path: 'secure/manuales/como-apagar-tu-mente.pdf',
+  },
+  'el-arte-de-creer-en-ti': {
+    filename: 'el-arte-de-creer-en-ti.pdf',
+    path: 'secure/manuales/el-arte-de-creer-en-ti.pdf',
+  },
+}
