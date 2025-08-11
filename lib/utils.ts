6f9b6c98e4564a0460d9c0cc7b6153186182f6dc
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-11 00:30:49 -0600

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,15 +4,4 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-{
-  name: "Cómo Apagar tu Mente",
-  description: "Workbook práctico para calmar el sobrepensamiento.",
-  price: 299,
-  image: "/como-apagar-tu-mente.png"
-},
-{
-  name: "El arte de creer en ti",
-  description: "Hojas de trabajo, scripts y plan de 6 sesiones.",
-  price: 349,
-  image: "/el arte de creer en ti.png"
-}
+
