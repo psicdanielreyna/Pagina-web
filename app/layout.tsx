325ea23cc69ff4bd3bbbdc47c73dfa1b9c30af67
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-13 20:12:04 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-// app/layout.tsx
 import "./globals.css"
 import { Inter } from "next/font/google"
 import Header from "@/components/header"
-import Footer from "@/components/footer" // si Footer también es default
+import Footer from "@/components/footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
