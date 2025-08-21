18b3fc07e74f7d3b204fac492732e75b0bfa998a
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-21 01:35:31 -0600

    Create blog.ts

---
+++ data/blog.ts
@@ -0,0 +1,33 @@
+import type { Post } from "@/types/content";
+
+export const postsHome: Post[] = [
+  {
+    slug: "el-arte-de-creer-en-ti",
+    title: "Post 1",
+    date: "8/10/2025",
+    excerpt:
+      "Pequeños cambios que fortalecen tu autoconfianza sin frases mágicas ni humo.",
+    href: "/blog/el-arte-de-creer-en-ti",
+    img: "/images/blog/el-arte-de-creer-en-ti.png",
+    alt: "Post 1",
+  },
+  {
+    slug: "ansiedad-3-pasos",
+    title: "Post 2",
+    date: "8/05/2025",
+    excerpt: "Un mini-protocolo para reconocer, regular y responder mejor.",
+    href: "/blog/ansiedad-en-3-pasos",
+    img: "/images/blog/ansiedad-3-pasos.png",
+    alt: "Post 2",
+  },
+  {
+    slug: "apagar-mente",
+    title: "Post 3",
+    date: "7/28/2025",
+    excerpt:
+      "Técnicas concretas para bajar el ruido mental cuando sientes que la cabeza no para.",
+    href: "/blog/como-apagar-tu-mente",
+    img: "/images/blog/apagar-mente.png",
+    alt: "Post 3",
+  },
+];
