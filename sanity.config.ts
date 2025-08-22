b298c1a614372f1328152091a94d5ba15cc1b1a7
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-22 16:19:30 -0600

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,1 +1,15 @@
-npm i next-sanity sanity @sanity/vision @sanity/image-url @portabletext/react groq
+// sanity.config.ts
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
+import { visionTool } from '@sanity/vision';
+import { schema } from './sanity/schema'; // tu schema combinado
+
+export default defineConfig({
+  name: 'default',
+  title: 'Studio Daniel',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  basePath: '/studio',
+  plugins: [structureTool(), visionTool()],
+  schema,
+});
