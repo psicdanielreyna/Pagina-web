140d0a3476063fe9fd9fefe9af8c485117144664
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-22 16:19:04 -0600

    Create layout.tsx

---
+++ app/studio/[[...index]]/layout.tsx
@@ -0,0 +1,4 @@
+// app/studio/[[...index]]/layout.tsx
+export default function StudioLayout({ children }: { children: React.ReactNode }) {
+  return <div className="min-h-screen">{children}</div>;
+}
