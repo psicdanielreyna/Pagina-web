54a393d2001876d5076245cb9098260122ab9428
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-22 16:05:25 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.cdn.sanity.io', // permite imágenes que vienen de Sanity
+      },
+    ],
+  },
 }
 
 export default nextConfig
-
