ed6afed7621503d66d886e5b9598effdb587c467
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-22 16:41:52 -0600

    Update index.ts

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,16 @@
+import { defineType } from "sanity";
 
+export const post = defineType({
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    { name: "title", type: "string", title: "Título" },
+    { name: "slug", type: "slug", title: "Slug", options: { source: "title" } },
+    { name: "date", type: "datetime", title: "Fecha" },
+    { name: "excerpt", type: "text", title: "Extracto" },
+    { name: "body", type: "array", of: [{ type: "block" }], title: "Contenido" },
+  ],
+});
+
+export const schemaTypes = [post];
