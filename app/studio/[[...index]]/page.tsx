e66595a2f847bcac6927a7a65312d16dccd6d648
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-22 16:18:42 -0600

    Update page.tsx

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -1,1 +1,9 @@
+// app/studio/[[...index]]/page.tsx
+'use client';
 
+import { NextStudio } from 'next-sanity/studio';
+import config from '../../../sanity.config'; // ajusta la ruta si tu sanity.config.ts está en otro lugar
+
+export default function StudioPage() {
+  return <NextStudio config={config} />;
+}
