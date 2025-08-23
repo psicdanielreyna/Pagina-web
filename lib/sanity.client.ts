48d2661771fa2a07bdec013a79b065168e24a0d8
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-22 21:38:15 -0600

    Update sanity.client.ts

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,8 +1,12 @@
+// lib/sanity.client.ts
 import { createClient } from "next-sanity";
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  useCdn: true, // usar caché para lecturas rápidas
-  apiVersion: "2023-01-01", // o la fecha de hoy
+  apiVersion: "2024-01-01",
+  useCdn: true,
 });
+
+// re-export con el nombre que espera el resto del código
+export const sanityClient = client;
