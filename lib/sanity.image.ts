a1d8651e6d7075167488f2ba0514daf1fdcfce75
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-23 00:01:05 -0600

    Update sanity.image.ts

--- lib/sanity.image.ts
+++ lib/sanity.image.ts
@@ -1,14 +1,12 @@
 // lib/sanity.image.ts
 import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url";
 import { client } from "./sanity.client";
 
 const builder = imageUrlBuilder(client);
 
-/**
- * Construye URLs de imágenes de Sanity.
- * Úsalo SOLO cuando el valor provenga de Sanity (image object o asset ref).
- */
-export function urlFor(source: SanityImageSource) {
+// Deducimos el tipo del primer parámetro de builder.image
+type ImageSource = Parameters<typeof builder.image>[0];
+
+export function urlFor(source: ImageSource) {
   return builder.image(source);
 }
