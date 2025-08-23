a7e68a79e075d6630f6eeffa5d997ec313fca32d
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-23 00:11:34 -0600

    Update sanity.image.ts

--- lib/sanity.image.ts
+++ lib/sanity.image.ts
@@ -1,12 +1,12 @@
-// lib/sanity.image.ts
 import imageUrlBuilder from "@sanity/image-url";
 import { client } from "./sanity.client";
 
 const builder = imageUrlBuilder(client);
 
-// Deducimos el tipo del primer parámetro de builder.image
-type ImageSource = Parameters<typeof builder.image>[0];
-
-export function urlFor(source: ImageSource) {
-  return builder.image(source);
+// Devuelve un builder; en el uso SIEMPRE termina con .url()
+export function urlFor(source: unknown) {
+  // si source viene vacío, devolvemos un builder “vacío” para no romper
+  // (igual en el JSX ponemos placeholder)
+  // @ts-ignore
+  return builder.image(source || {});
 }
