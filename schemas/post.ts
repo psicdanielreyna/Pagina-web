653fee2a9d71252977c5c085e3ea51b031b5af2a
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-22 21:13:29 -0600

    Update post.ts

--- schemas/post.ts
+++ schemas/post.ts
@@ -1,11 +1,11 @@
 const post = {
-  name: "post",
-  title: "Post",
-  type: "document",
+  name: 'post',
+  title: 'Post',
+  type: 'document',
   fields: [
-    { name: "title", type: "string", title: "Título" },
-    { name: "slug", type: "slug", title: "Slug", options: { source: "title" } },
-    { name: "content", type: "text", title: "Contenido" },
+    {name: 'title', type: 'string', title: 'Título'},
+    {name: 'slug', type: 'slug', title: 'Slug', options: {source: 'title'}},
+    {name: 'content', type: 'text', title: 'Contenido'},
   ],
 };
 
