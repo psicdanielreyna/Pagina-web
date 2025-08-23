f5e17509d0b4dd938397f9d60266864b4cd78ee0
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-22 21:09:42 -0600

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,24 +1,18 @@
-// sanity.config.ts
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import schemas from './schemas'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+
+import schemas from "./schemas"; // aquí importa tu carpeta de esquemas
 
 export default defineConfig({
-  name: 'default',
-  title: 'Mi Blog',
-  // Asegúrate de tener estos envs en Netlify:
-  // NEXT_PUBLIC_SANITY_PROJECT_ID y NEXT_PUBLIC_SANITY_DATASET
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  name: "default",
+  title: "Mi Blog Daniel Reyna",
 
-  // 👇 Nada de "studio" aquí. Solo las tools soportadas:
-  plugins: [
-    structureTool(),
-    visionTool(),
-  ],
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, 
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 
+  plugins: [structureTool(), visionTool()],
   schema: {
     types: schemas,
   },
-})
+});
