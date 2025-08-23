00a3794c9e20a55d3b7223ead775f92bd0eddb54
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-22 20:45:47 -0600

    Update index.ts

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,16 +2,3 @@
 import post from "./post";
 
 export default [post];
-  name: "post",
-  title: "Post",
-  type: "document",
-  fields: [
-    { name: "title", type: "string", title: "Título" },
-    { name: "slug", type: "slug", title: "Slug", options: { source: "title" } },
-    { name: "date", type: "datetime", title: "Fecha" },
-    { name: "excerpt", type: "text", title: "Extracto" },
-    { name: "body", type: "array", of: [{ type: "block" }], title: "Contenido" },
-  ],
-});
-
-export const schemaTypes = [post];
