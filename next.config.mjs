63931a739ec55de6687d6f6d75f4e3631750cffe
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-24 01:59:23 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "cdn.sanity.io" },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
     ],
   },
 };
+
 export default nextConfig;
