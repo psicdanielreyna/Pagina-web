b3123dcc1fc96a85c1d9c15e7010e7b45cc09811
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-23 19:55:39 -0600

    Create site.ts

---
+++ lib/site.ts
@@ -0,0 +1,15 @@
+export const siteNav = [
+  { href: "/", label: "Inicio" },
+  { href: "/servicios", label: "Servicios" },
+  { href: "/tienda", label: "Tienda" },
+  { href: "/blog", label: "Blog" },
+  { href: "/sobre-mi", label: "Sobre mí" },
+  { href: "/agenda", label: "Agenda" },
+];
+
+export const socialLinks = [
+  { href: "https://www.instagram.com/psic.danielreyna/", label: "Instagram", icon: "instagram" },
+  { href: "https://www.facebook.com/psic.danielreyna/", label: "Facebook", icon: "facebook" },
+  { href: "https://www.youtube.com/@PsicDanielReyna", label: "YouTube", icon: "youtube" },
+  { href: "https://x.com/psicdanreyna", label: "X / Twitter", icon: "twitter" },
+];
