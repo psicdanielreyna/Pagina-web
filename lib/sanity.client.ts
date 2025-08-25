9697e43badccf6da1ada9a83030f3c80eb61dd6e
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-24 18:03:47 -0600

    Update sanity.client.ts

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,9 +1,8 @@
-// lib/sanity.client.ts
 import { createClient } from "next-sanity";
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: "2025-01-01",
-  useCdn: true, // usa CDN para lecturas públicas
+  apiVersion: "2025-01-01", // o la fecha que prefieras
+  useCdn: true, // true = cacheado en edge (más rápido, pero puede no ser inmediato)
 });
