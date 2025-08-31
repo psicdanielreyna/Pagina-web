08a8e1c0a48214bffa8851d8f9b5f538902df44a
psicdanielreyna <psic.danielreyna@gmail.com>
2025-08-30 22:45:44 -0600

    Delete lib/sanity.client.ts

--- lib/sanity.client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: "2025-01-01", // o la fecha que prefieras
-  useCdn: true, // true = cacheado en edge (más rápido, pero puede no ser inmediato)
-});
