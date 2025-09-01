f0efbb90ddb611d6629d925c2024ea79941d502d
psicdanielreyna <psic.danielreyna@gmail.com>
2025-09-01 06:01:17 +0000

    chore: remove Sanity Studio route and config

--- app/studio/[[...index]]/layout.tsx
+++
@@ -1,4 +0,0 @@
-// app/studio/[[...index]]/layout.tsx
-export default function StudioLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-screen">{children}</div>;
-}
--- app/studio/[[...index]]/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/studio/[[...index]]/page.tsx
-'use client'
-
-import {NextStudio} from 'next-sanity/studio'
-import config from '../../../sanity.config'
-
-export default function StudioPage() {
-  return <NextStudio config={config} />
-}
