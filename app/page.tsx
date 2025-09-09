3badd1149374ac54490a8c319b2b7d94a8a324ce
psicdanielreyna <psic.danielreyna@gmail.com>
2025-09-08 20:23:41 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import Hero from "@/components/Hero";
 import Recursos from "@/components/Recursos";
 import UltimoBlog from "@/components/UltimoBlog";
 import Newsletter from "@/components/Newsletter";
+import LatestBlog from "@/components/LatestBlog";
 
 export default function HomePage() {
   return (
     <div className="space-y-16">
       <Hero />
       <Recursos />
-      <UltimoBlog />
+      <LatestBlog />
       <Newsletter />   {/* <--- Newsletter abajo del blog */}
     </div>
   );
