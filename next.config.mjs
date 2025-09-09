0e235754519049591c806c9e4f38a1c08fd8ed6d
psicdanielreyna <psic.danielreyna@gmail.com>
2025-09-08 18:40:52 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    // Agrega aquí los dominios desde donde sirves imágenes
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-      },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "danielreyna.com" },
+      { protocol: "https", hostname: "assets.st-note.com" }, // ej. si lo necesitas
+      // { protocol: "https", hostname: "tu-cdn.com" },
     ],
   },
 };
