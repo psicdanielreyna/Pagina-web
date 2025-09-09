797f3d9c4f8302bb7a07af5fcedef1864fae1896
psicdanielreyna <psic.danielreyna@gmail.com>
2025-09-08 22:12:18 -0600

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
 // components/header.tsx
-import SiteHeader from "@/components/site-header";
+import SiteHeader from "@/components/SiteHeader";
 
 export default function Header() {
   return <SiteHeader />;
