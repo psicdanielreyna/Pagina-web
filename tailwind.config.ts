01339a42b31562f4f6e166d3dfe7b3bf2e8d58d7
psicdanielreyna <psic.danielreyna@gmail.com>
2025-09-08 19:42:30 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,13 @@
-// tailwind.config.ts o .js
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: ["./app/**/*.{ts,tsx,js,jsx,mdx}", "./components/**/*.{ts,tsx,js,jsx,mdx}", "./lib/**/*.{ts,tsx,js,jsx,mdx}"],
+// tailwind.config.js
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,js,jsx}", "./components/**/*.{ts,tsx,js,jsx}", "./lib/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {
       colors: {
         almond: "#F6E9D9",
         evergreen: "#043222",
       },
-      typography: {
-        DEFAULT: {
-          css: {
-            color: "#043222",
-            a: { textDecoration: "underline" },
-            "h1,h2,h3,h4": { color: "#043222" },
-          },
-        },
-      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
 };
-
-export default config;
