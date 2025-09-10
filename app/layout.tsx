a5934b9b6d8ae3b27771523a561e7940addbd070
psicdanielreyna <psic.danielreyna@gmail.com>
2025-09-10 02:28:44 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import Header from "@/components/Header";
+import Header from "@/components/SiteHeader";
 import Footer from "@/components/Footer";
 import Newsletter from "@/components/Newsletter"; // 👈 aquí
 
@@ -15,15 +15,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <body>
-        <Header />
-
+        <SiteHeader />
         <main className="min-h-[60vh]">{children}</main>
-
-        {/* Newsletter global (una sola vez) */}
         <section className="container mx-auto max-w-5xl px-4 md:px-6 my-16">
           <Newsletter />
         </section>
-
         <Footer />
       </body>
     </html>
