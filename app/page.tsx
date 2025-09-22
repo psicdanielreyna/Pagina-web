92f99ab4e49aa0d071c6e55a4ce07040ca19bbb0
psicdanielreyna <psic.danielreyna@gmail.com>
2025-09-22 16:37:31 -0600

    Update page.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-// app/page.tsx
 import Hero from "@/components/Hero";
 import Recursos from "@/components/Recursos";
 import LatestBlog from "@/components/LatestBlog";
 
 export default function HomePage() {
   return (
-    <div className="space-y-16">
+    <div className="space-y-16 mb-16"> {/* 👈 separa blog del footer */}
       <Hero />
       <Recursos />
       <LatestBlog />
